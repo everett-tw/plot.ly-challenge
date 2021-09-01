ea5f10e237bf14e6948a474e755c70d740c58891
everett.tw <everett.waterman@gmail.com>
2021-08-31 21:12:12 -0700

    added d3 select function to operate dropdown

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,20 +1,70 @@
 const samples = "https://raw.githubusercontent.com/everett-tw/plot.ly-challenge/main/data/samples.json"
 
-var array_object = []
-d3.json(samples).then(function(data){
-    console.log(data);
+function graph_all_the_things(this_sample,this_metadata){
     //https://plotly.com/javascript/horizontal-bar-charts/
-    
     var bar_data = [{
         type: 'bar',
-        x: data.samples[0].sample_values.slice(0,10),
-        y: data.samples[0].otu_ids.slice(0,10).map(),
-        text: data.samples[0].otu_labels.slice(0,10),
+        x: this_sample.sample_values.slice(0,10),
+        y: this_sample.otu_ids.slice(0,10).map(x => 'OTU '+ x),
+        text: this_sample.otu_labels.slice(0,10),
         orientation: 'h'
     }];
 
+    //.map(x => 'OTU'+ x)
     Plotly.newPlot('bar', bar_data);
-    
-    var array_object = data.samples
+
+    //https://plotly.com/javascript/bubble-charts/
+    var trace1 = {
+        x: this_sample.otu_ids,
+        y: this_sample.sample_values,
+        text: this_sample.otu_labels,
+        mode: 'markers',
+        marker: {
+            color: 'rgb(31, 119, 180)',
+            opacity: 0.6,
+            size: this_sample.sample_values
+        }
+    };
+    var layout = {
+        title: 'OTU ID',
+        showlegend: false,
+        // height: 600,
+        // width: 600
+    };
+    var bubble_data = [trace1];
+    Plotly.newPlot('bubble', bubble_data, layout);
+
+    sample_metadata = d3.select('#sample-metadata');
+    sample_metadata.selectAll('p').remove()
+    for(key in this_metadata)
+          sample_metadata.append('p').text(`${key}:${this_metadata[key]}`);
+};
+
+
+let array_object = []
+let metadata_object = [];
+
+d3.json(samples).then(function(data){
+    console.log(data);
+    array_object = data.samples;
+    metadata_object = data.metadata;
+    user_input();
 });
-//https://plotly.com/javascript/bubble-charts/
+
+d3.selectAll("#selDataset").on("change", updatePlotly);
+
+function user_input() {
+    graph_all_the_things(array_object[0], metadata_object[0]);
+    dropdown = d3.select('#selDataset')
+    array_object.forEach((value, i) =>
+        dropdown.append('option').attr('value',i).text(value.id)
+    )
+  }
+
+
+function updatePlotly() {
+    var dropdownMenu = d3.select("#selDataset");
+    var curr_belly = dropdownMenu.property("value");
+    graph_all_the_things(array_object[curr_belly], metadata_object[curr_belly]);
+}
+
